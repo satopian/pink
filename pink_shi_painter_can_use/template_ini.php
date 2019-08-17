377344fefb157fdc35a7d96366b618427b398e9d
satopian <satopian@mia.eek.jp>
2019-08-18 00:46:29 +0900

    lot.190818

--- pink_shi_painter_can_use/template_ini.php
+++ pink_shi_painter_can_use/template_ini.php
@@ -1,11 +1,11 @@
 <?php
 /*
-  * Template - PINK - lot.190805  by さとぴあ  >> http://stp.sblo.jp/
+  * Template - PINK - lot.190818  by さとぴあ  >> http://stp.sblo.jp/
   *
 */
 
 //テンプレートのバージョン
-define('TEMPLATE_VER', "lot.190816");
+define('TEMPLATE_VER', "lot.190818");
 
 
 /* ---------- ADD:2004/06/22 ---------- */
