772e9fede8909cabc35847d3fba5437755b404ff
satopian <satopian@mia.eek.jp>
2019-09-10 23:54:52 +0900

    lot.190910

--- pink_neo_only/template_ini.php
+++ pink_neo_only/template_ini.php
@@ -1,11 +1,11 @@
 <?php
 /*
-  * Template - PINK - lot.190818  by さとぴあ  >> http://stp.sblo.jp/
+  * Template - PINK - lot.190910  by さとぴあ  >> http://stp.sblo.jp/
   *
 */
 
 //テンプレートのバージョン
-define('TEMPLATE_VER', "lot.190818");
+define('TEMPLATE_VER', "lot.190910");
 
 
 /* ---------- ADD:2004/06/22 ---------- */
